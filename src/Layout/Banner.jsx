9e0131c4c7d8efe35fec8e1eba72d122ab8bd658
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-05-10 16:48:02 +0600

    Banner Layout design

--- src/Layout/Banner.jsx
+++ src/Layout/Banner.jsx
@@ -5,26 +5,34 @@ import {
   Typography,
   Button,
 } from "@material-tailwind/react";
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa6";
 export default function Banner() {
   return (
     <section className="   bg-[#f0f2f5]">
    
    <div className=" max-w-container mx-auto py-20">
-   <div>
+   <div className=" relative">
     <img className=" rounded-lg" src="https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-seo-grid-01.jpg" alt="banner" />
-    <Card className="mt-6 w-[580px]">
+    <Card className="mt-6 w-[580px] pb-6 absolute top-[62%] left-14">
     <CardBody>
         <a className=" text-[#da474e] cursor-pointer py-3">RESEARCH</a>
        <h4 className="pt-3 font-semibold text-3xl text-black">How to write a title with an optimal width for SEO</h4>
       </CardBody>
-
       <CardFooter className="pt-0">
-     <div className=" flex gap-3">
-      <img  className=" w-30 rounded-full" src="https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/author/author-image-2.png" alt="" />
-      <div>
+     <div className=" flex items-center justify-between ">
+    <div className=" flex gap-x-3">
+    <img  className=" w-30 rounded-full" src="https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/author/author-image-2.png" alt="" />
+      <div >
         <h5>Rahabi Khan</h5>
-        <p>Feb 17, 2019 300k Views</p>
+        <p>Feb 17, 2019 300k Views</p>        
       </div>
+    </div>
+      <div className=" flex gap-x-4 ">
+          <FaFacebook/>
+          <FaInstagram/>
+          <FaTwitter/>
+          <FaLinkedin/>
+        </div>
      </div>
       </CardFooter>
     </Card>
