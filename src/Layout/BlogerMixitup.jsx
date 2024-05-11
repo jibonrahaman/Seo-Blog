817e397441758a3e0272902b393d76bc383d1d9f
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-05-11 15:34:32 +0600

    BlogerMixitup-card design

--- src/Layout/BlogerMixitup.jsx
+++ src/Layout/BlogerMixitup.jsx
@@ -1,6 +1,9 @@
 import Container from "../Components/Container"
 import Flex from "../Components/Flex"
 import Heading from "../Components/Heading"
+import HoverHiddenShoData from "../Components/HoverHiddenShoData"
+import HoverUnderline from "../Components/HoverUnderline"
+import ScaleImg from "../Components/ScaleImg"
 export default function BlogerMixitup() {
   return (
     <section>
@@ -11,6 +14,20 @@ export default function BlogerMixitup() {
             <button className="px-8 cursor-pointer py-3 bg-white text-black rounded-lg shadow-shadow border">Research</button>
             <button className="px-8 cursor-pointer py-3 bg-white text-black rounded-lg shadow-shadow border">Marketing</button>
             <button className="px-8 cursor-pointer py-3 bg-white text-black rounded-lg shadow-shadow border">Branding</button>
+         </Flex>
+         <Flex className=" gap-x-10 justify-between">
+
+          <div className=" bg-[#f0f2f5]  group rounded-xl ">
+          <div className=" p-10 flex flex-col w-[400px] items-center">
+                <HoverHiddenShoData dataText="GADGETS"  text='GADGETS' />
+                <HoverUnderline  className=" text-center group-hover:before:w-[300px]" text="Search improvements in Content Explorer" />
+            </div>
+            <div className=" relative overflow-hidden">
+                <img className="w-full absolute top-0 left-0 z-10 block" src="https://new.axilthemes.com/demo/template/blogar/assets/images/icons/shape-01.png" alt="a" />
+                <ScaleImg className=" w-full" src="https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-11.jpg" alt="mixi-1" />
+            </div>
+          </div>
+
          </Flex>
                  </Container>
     </section>
