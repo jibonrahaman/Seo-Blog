417845f627c7e8893d002b41cf6a58615e13efe5
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-05-11 22:34:23 +0600

    slick slider

--- src/Data/Blogmixi.js
+++ src/Data/Blogmixi.js
@@ -1,53 +1,54 @@
 const BlogerMixi = [
     {
+    
     test:"GADGETS",
     dataText : "GADGETS",
      underline : "Search improvements in Content Explorer",
      img : "https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-11.jpg" ,
-     title: "Search "
+     title: "Search"
+     
     },
     {
     test:"ACCESSIBILITY",
     dataText : "ACCESSIBILITY",
      underline : "Lightweight,grippable,and ready to go.",
      img : "https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-01.jpg" ,
-     title: "Search "
+     title: "Search"
     },
      {
     test:"ACCESSIBILITY",
     dataText : "ACCESSIBILITY",
      underline : "Introducing Keyword Data in Site Explorer",
      img : "https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-09.jpg" ,
-     title: "Search "
+     title: "Search"
     },
       {
     test:"APPLE DESIGN",
     dataText : "APPLE DESIGN",
      underline : "Total monthly search traffic for API.",
      img : "https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-10.jpg" ,
-     title: "Search "
+     title: "Search"
     },
-
       {
     test:"APPLE DESIGN",
     dataText : "APPLE DESIGN",
      underline : "Bold new experience. Same Mac magic.",
      img : "https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-02.jpg" ,
-     title: "Research "
+     title: "Research"
     }, 
       {
     test:"CASE STUDY",
     dataText : "CASE STUDY",
      underline : "Lightweight, grippable,and ready to go.",
      img : "https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-03.jpg" ,
-     title: "Research "
+     title: "Research"
     },
     {
         test:"ACCESSIBILITY",
         dataText : "ACCESSIBILITY",
          underline : "Lightweight,grippable,and ready to go.",
          img : "https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-01.jpg" ,
-         title: "Research "
+         title: "Research"
         },
         
         {
@@ -55,56 +56,56 @@ const BlogerMixi = [
         dataText : "DESIGN",
          underline : "Lightweight,grippable,and ready to go.",
          img : "https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-08.jpg" ,
-         title: "Marketing "
+         title: "Marketing"
         },
         {
             test:"GADGETS",
             dataText : "GADGETS",
              underline : "Search improvements in Content Explorer",
              img : "https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-11.jpg" ,
-             title: "Marketing "
+             title: "Marketing"
             },
             {
             test:"ACCESSIBILITY",
             dataText : "ACCESSIBILITY",
              underline : "Lightweight,grippable,and ready to go.",
              img : "https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-01.jpg" ,
-             title: "Marketing "
+             title: "Marketing"
             },
              {
             test:"ACCESSIBILITY",
             dataText : "ACCESSIBILITY",
              underline : "Introducing Keyword Data in Site Explorer",
              img : "https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-09.jpg" ,
-             title: "Marketing "
+             title: "Marketing"
             },
              {
             test:"GADGETS",
             dataText : "GADGETS",
              underline : "Creative Game With The New DJI Mavic Air 2",
              img : "https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-02.jpg" ,
-             title: "Branding "
+             title: "Branding"
             }, 
             {
             test:"APPLE DESIGN",
             dataText : "APPLE DESIGN",
              underline : "Bold new experience. Same Mac magic.",
              img : "https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-03.jpg" ,
-             title: "Branding "
+             title: "Branding"
             },
             {
                 test:"GADGETS",
                 dataText : "GADGETS",
                  underline : "Lightweight,grippable,and ready to go.",
                  img : "https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-01.jpg" ,
-                 title: "Branding "
+                 title: "Branding"
                 },
                 {
                 test:"GADGETS",
                 dataText : "GADGETS",
                  underline : "Lightweight,grippable,and ready to go.",
                  img : "https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-01.jpg" ,
-                 title: "Branding "
+                 title: "Branding"
                 }
 ]
 export default BlogerMixi
--- src/Layout/BlogerMixitup.jsx
+++ src/Layout/BlogerMixitup.jsx
@@ -6,23 +6,43 @@ import HoverHiddenShoData from "../Components/HoverHiddenShoData"
 import HoverUnderline from "../Components/HoverUnderline"
 import ScaleImg from "../Components/ScaleImg"
 import BlogerMixi from "../Data/Blogmixi"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+
 export default function BlogerMixitup() {
-    const [Data,setData] = useState(BlogerMixi)
-    const filt = Data.filter((item)=>{
-        console.log(item.title);
-    })
+    const [Datas,setDatas] = useState(BlogerMixi)
+    const [Data,setData] = useState([])
+   
+    const handleClickMixi = (name)=>{
+      const filt =  Datas.filter((item)=>{
+            if(item.title === name){
+               return item
+            }
+        })
+        setData(filt)    
+    }
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      };
     return (
     <section>
         <Container className=" mx-auto py-5">
          <Heading text="What's new at Bloger"/>
          <Flex className=" gap-x-8 py-10">
-            <button className="px-8 cursor-pointer py-3 bg-white text-black rounded-lg shadow-shadow border">Search Engines</button>
-            <button className="px-8 cursor-pointer py-3 bg-white text-black rounded-lg shadow-shadow border">Research</button>
-            <button className="px-8 cursor-pointer py-3 bg-white text-black rounded-lg shadow-shadow border">Marketing</button>
-            <button className="px-8 cursor-pointer py-3 bg-white text-black rounded-lg shadow-shadow border">Branding</button>
+            <button onClick={()=>handleClickMixi("Search")} className="px-8 cursor-pointer py-3 bg-white text-black rounded-lg shadow-shadow border">Search Engines</button>
+            <button onClick={()=>handleClickMixi("Research")} className="px-8 cursor-pointer py-3 bg-white text-black rounded-lg shadow-shadow border">Research</button>
+            <button onClick={()=>handleClickMixi("Marketing")} className="px-8 cursor-pointer py-3 bg-white text-black rounded-lg shadow-shadow border">Marketing</button>
+            <button onClick={()=>handleClickMixi("Branding")} className="px-8 cursor-pointer py-3 bg-white text-black rounded-lg shadow-shadow border">Branding</button>
          </Flex>
-         <Flex className=" gap-x-10 justify-between">
-          <div className=" bg-[#f0f2f5]  group rounded-xl ">
+        
+        
+         <Slider {...settings}>
+         <div >
+         <div className="w-[400px] bg-[#f0f2f5]  group rounded-xl ">
           <div className=" p-10 flex flex-col w-[400px] items-center">
                 <HoverHiddenShoData dataText="GADGETS"  text='GADGETS' />
                 <HoverUnderline  className=" text-center group-hover:before:w-[300px]" text="Search improvements in Content Explorer" />
@@ -32,7 +52,34 @@ export default function BlogerMixitup() {
                 <ScaleImg className=" w-full" src="https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-11.jpg" alt="mixi-1" />
             </div>
           </div>
-         </Flex>
+         </div>
+          <div >
+         <div className="mx-3 w-[400px] bg-[#f0f2f5]  group rounded-xl ">
+          <div className=" p-10 flex flex-col w-[400px] items-center">
+                <HoverHiddenShoData dataText="GADGETS"  text='GADGETS' />
+                <HoverUnderline  className=" text-center group-hover:before:w-[300px]" text="Search improvements in Content Explorer" />
+            </div>
+            <div className=" relative overflow-hidden">
+                <img className="w-full absolute top-0 left-0 z-10 block" src="https://new.axilthemes.com/demo/template/blogar/assets/images/icons/shape-01.png" alt="a" />
+                <ScaleImg className=" w-full" src="https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-11.jpg" alt="mixi-1" />
+            </div>
+          </div>
+         </div>
+          <div >
+         <div className="mx-3 w-[400px] bg-[#f0f2f5]  group rounded-xl ">
+          <div className=" p-10 flex flex-col w-[400px] items-center">
+                <HoverHiddenShoData dataText="GADGETS"  text='GADGETS' />
+                <HoverUnderline  className=" text-center group-hover:before:w-[300px]" text="Search improvements in Content Explorer" />
+            </div>
+            <div className=" relative overflow-hidden">
+                <img className="w-full absolute top-0 left-0 z-10 block" src="https://new.axilthemes.com/demo/template/blogar/assets/images/icons/shape-01.png" alt="a" />
+                <ScaleImg className=" w-full" src="https://new.axilthemes.com/demo/template/blogar/assets/images/post-images/post-column-11.jpg" alt="mixi-1" />
+            </div>
+          </div>
+         </div>
+       </Slider>
+        
+        
                  </Container>
     </section>
   )
