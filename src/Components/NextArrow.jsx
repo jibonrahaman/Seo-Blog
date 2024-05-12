b8e156e3657b12b584bfaf7a3052f42f3c1ce70b
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-05-12 20:41:26 +0600

    translate top solution

--- src/Components/NextArrow.jsx
+++ src/Components/NextArrow.jsx
@@ -3,7 +3,7 @@ import { LuArrowLeftToLine } from 'react-icons/lu'
 const NextArrow = (props) => {
     const { className, style, onClick } = props;
     return (
-        <div className={`flex justify-center items-center z-10  w-11 h-11 text-[#FFFFFF] rounded-full bg-[#979797] cursor-pointer  absolute top-1/3 left-4`} onClick={onClick}><LuArrowLeftToLine size="20" /> </div>
+        <div className={`flex justify-center items-center z-10  w-11 h-11 text-[#FFFFFF] rounded-full bg-[#979797] cursor-pointer  absolute top-1/3 left-1 translate-y-[50%]`} onClick={onClick}><LuArrowLeftToLine size="20" /> </div>
 
     );
 }
